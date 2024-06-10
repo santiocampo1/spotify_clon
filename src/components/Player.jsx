692c67a9eabe8aa98038ee6cda6c7f8364613160
santiocampo1 <sanntiocampo@gmail.com>
2024-06-10 10:55:57 -0300

    Problems with audio fixed

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -10,6 +10,14 @@ export const Play = () => (
     <svg role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16"><path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z"></path></svg>
 )
 
+export const VolumeSilence = () => (
+    <svg fill="currentColor" role="presentation" height="16" width="16" aria-hidden="true" aria-label="Volumen apagado" viewBox="0 0 16 16" ><path d="M13.86 5.47a.75.75 0 0 0-1.061 0l-1.47 1.47-1.47-1.47A.75.75 0 0 0 8.8 6.53L10.269 8l-1.47 1.47a.75.75 0 1 0 1.06 1.06l1.47-1.47 1.47 1.47a.75.75 0 0 0 1.06-1.06L12.39 8l1.47-1.47a.75.75 0 0 0 0-1.06z"></path><path d="M10.116 1.5A.75.75 0 0 0 8.991.85l-6.925 4a3.642 3.642 0 0 0-1.33 4.967 3.639 3.639 0 0 0 1.33 1.332l6.925 4a.75.75 0 0 0 1.125-.649v-1.906a4.73 4.73 0 0 1-1.5-.694v1.3L2.817 9.852a2.141 2.141 0 0 1-.781-2.92c.187-.324.456-.594.78-.782l5.8-3.35v1.3c.45-.313.956-.55 1.5-.694V1.5z"></path></svg>
+)
+
+export const Volume = () => (
+    <svg fill="currentColor" role="presentation" height="16" width="16" aria-hidden="true" aria-label="Volumen alto" id="volume-icon" viewBox="0 0 16 16"><path d="M9.741.85a.75.75 0 0 1 .375.65v13a.75.75 0 0 1-1.125.65l-6.925-4a3.642 3.642 0 0 1-1.33-4.967 3.639 3.639 0 0 1 1.33-1.332l6.925-4a.75.75 0 0 1 .75 0zm-6.924 5.3a2.139 2.139 0 0 0 0 3.7l5.8 3.35V2.8l-5.8 3.35zm8.683 4.29V5.56a2.75 2.75 0 0 1 0 4.88z"></path><path d="M11.5 13.614a5.752 5.752 0 0 0 0-11.228v1.55a4.252 4.252 0 0 1 0 8.127v1.55z"></path></svg>
+)
+
 const CurrentSong = ({ image, title, artists }) => {
     // console.log({ image, title });
 
@@ -31,8 +39,30 @@ const CurrentSong = ({ image, title, artists }) => {
     )
 }
 
+const VolumeControl = () => {
+    const volume = usePlayerStore(state => state.volume);
+    const setVolume = usePlayerStore(state => state.setVolume);
+
+    return (
+        <div className='flex justify-center gap-x-2'>
+            {volume < 0.1 ? <VolumeSilence /> : <Volume />}
+            <Slider
+                defaultValue={[100]}
+                max={100}
+                min={0}
+                className='w-[95px]'
+                onValueChange={(value) => {
+                    const [newVolume] = value;
+                    const volumeValue = newVolume / 100;
+                    setVolume(volumeValue);
+                }}
+            />
+        </div>
+    )
+}
+
 const Player = () => {
-    const { currentMusic, isPlaying, setIsPlaying } = usePlayerStore(state => state);
+    const { currentMusic, isPlaying, setIsPlaying, volume } = usePlayerStore(state => state);
     const audioRef = useRef();
     const volumeRef = useRef(1);
 
@@ -41,12 +71,16 @@ const Player = () => {
     }, [isPlaying])
 
     useEffect(() => {
+        audioRef.current.volume = volume;
+    }, [volume]);
+
+    useEffect(() => {
         const { song, playlist, songs } = currentMusic;
 
         if (song) {
             const src = `/music/${playlist?.id}/0${song.id}.mp3`
             audioRef.current.src = src;
-            audioRef.current.volume = volumeRef.current;
+            audioRef.current.volume = volume;
             audioRef.current.play();
         }
 
@@ -71,20 +105,8 @@ const Player = () => {
                 </div>
             </div>
 
-            <div className='grid place-content-center text-white'>
-                <Slider
-                defaultValue={[100]} 
-                max={100} 
-                min={0} 
-                className='w-[95px]'
-                onValueChange={(value) => {
-                    const [newVolume] = value;
-                    const volumeValue = newVolume / 100;
-                    volumeRef.current = volumeValue;
-                    audioRef.current.volume = volumeValue;
-                }}
-                />
-
+            <div className='grid place-content-center'>
+                <VolumeControl />
             </div>
 
             <audio ref={audioRef}></audio>
