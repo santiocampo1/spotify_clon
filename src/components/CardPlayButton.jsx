769316bd151256ae17e870999414fe46eb7fd69f
santiocampo1 <sanntiocampo@gmail.com>
2024-06-11 12:17:23 -0300

    Play button on playlist page

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Pause, Play } from './Player';
 import { usePlayerStore } from '../store/playerStore';
 
-const CardPlayButton = ({ id }) => {
+const CardPlayButton = ({ id, size = 'small' }) => {
   const {
     currentMusic,
     isPlaying,
@@ -33,7 +33,7 @@ const CardPlayButton = ({ id }) => {
   }
 
   return (
-    <button onClick={handleClick} className='card-play-button rounded-full bg-green-500 p-4'>
+    <button onClick={handleClick} className='card-play-button rounded-full bg-green-500 p-4 hover:scale-105 transition hover:bg-green-400'>
       {isPlayingPlaylist ? <Pause /> : <Play />}
     </button>
   )
