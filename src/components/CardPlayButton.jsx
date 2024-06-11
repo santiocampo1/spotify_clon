a2359a40b06462c3072b7bb328652549af0178d4
santiocampo1 <sanntiocampo@gmail.com>
2024-06-11 12:24:48 -0300

    Size of the play icon

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -9,32 +9,34 @@ const CardPlayButton = ({ id, size = 'small' }) => {
     setIsPlaying,
     setCurrentMusic
   } = usePlayerStore(state => state);
-  
+
   const isPlayingPlaylist = isPlaying && currentMusic?.playlist.id === id;
 
   const handleClick = () => {
 
-    if(isPlayingPlaylist) {
+    if (isPlayingPlaylist) {
       setIsPlaying(false);
       return;
-    } 
+    }
 
     //* Promise
     fetch(`/api/get-info-playlist.json?id=${id}`)
-    .then(res => res.json())
-    .then(data => {
-      const { songs, playlist } = data; 
-      
-      setIsPlaying(true);
-      setCurrentMusic({songs, playlist, song: songs[0]})
+      .then(res => res.json())
+      .then(data => {
+        const { songs, playlist } = data;
 
-      console.log({songs, playlist});
-    })
+        setIsPlaying(true);
+        setCurrentMusic({ songs, playlist, song: songs[0] })
+
+        console.log({ songs, playlist });
+      })
   }
+
+  const iconClassName = size === 'small' ? 'w-4 h-4' : 'w-5 h-5';
 
   return (
     <button onClick={handleClick} className='card-play-button rounded-full bg-green-500 p-4 hover:scale-105 transition hover:bg-green-400'>
-      {isPlayingPlaylist ? <Pause /> : <Play />}
+      {isPlayingPlaylist ? <Pause className={iconClassName} /> : <Play className={iconClassName} />}
     </button>
   )
 }
